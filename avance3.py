3080389dc1ba2ada8115413ffd412bd7d6da0fd2
SaVa-dev <santymatusg@gmail.com>
2024-04-12 20:46:25 -0600

    Se eliminio el csv y se agregó el correcto, se corrigió la query tarifas y se agrego la query para agregar el csv a sql

--- avance3.py
+++ avance3.py
@@ -1,5 +1,12 @@
+import mysql.connector
+
 def main() -> None:
-    
+    ds = mysql.connector.connect(
+        host        = 'localhost',
+        user        = 'root',
+        password    = '',
+        database    = ''
+    )
     pass
 
 if __name__ == '__main__':
--- tabla.sql
+++ tabla.sql
@@ -1,16 +1,25 @@
 CREATE TABLE Tarifas (
-    zona VARCHAR(15)
+    zona_inyeccion VARCHAR(50),
+    zona_extraccion VARCHAR(50),
+    capacidad_base_firme DECIMAL(10, 5),
+    uso_base_firme DECIMAL(10, 5),
+    capacidad_base_firme_temporal DECIMAL(10, 5),
+    uso_base_firme_temporal DECIMAL(10, 5),
+    capacidad_base_interrumpible DECIMAL(10, 5),
+    uso_base_interrumpible DECIMAL(10, 5),
+    volumetrica DECIMAL(10, 5),
+    fecha_inicio DATE,
+    fecha_fin DATE
+);
 
-    capacidad_base_firme NUMERIC(1, 9)
-    uso_base_firme       NUMERIC(1, 9)
-
-    capacidad_base_temporal NUMERIC(1, 9)
-    uso_base_temporal       NUMERIC(1, 9)
-
-    maxima_base_interrumpible NUMERIC(1, 9)
-    minima_base_interrumpible NUMERIC(1, 9)
-    
-    volumetrica NUMERIC(1, 9)
-    fecha_inicio DATE
-    fecha_fin    DATE
-)
+LOAD DATA INFILE '/home/sava/Documents/Evidencia-BigData/Avance3/Tarifas por puntos 2016-2017(3).csv'
+INTO TABLE Tarifas
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(zona_inyeccion, zona_extraccion, capacidad_base_firme, uso_base_firme,
+capacidad_base_firme_temporal, uso_base_firme_temporal,
+capacidad_base_interrumpible, uso_base_interrumpible, volumetrica,
+@fecha_inicio, @fecha_fin)
+SET fecha_inicio = STR_TO_DATE(@fecha_inicio, '%d/%m/%Y'),
+    fecha_fin = STR_TO_DATE(@fecha_fin, '%d/%m/%Y');
