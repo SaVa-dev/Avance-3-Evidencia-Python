dd01c8c644aa9006ed265792166320c7b393a7cb
SaVa-dev <santymatusg@gmail.com>
2024-04-12 23:18:22 -0600

    se agregó la función de obtener los datos de las tablas de SQL, y guardarlos en una lista de tuplas

--- avance3.py
+++ avance3.py
@@ -1,6 +1,8 @@
+import matplotlib.pyplot as plt
 import mysql.connector
 import textwrap
 
+# Main del programa
 def main() -> None:
 
     # Conectar a la base de datos de phpMyAdmin (xampp)
@@ -14,11 +16,23 @@ def main() -> None:
     # Crear un cursor para mandar querys desde python
     cursor = db.cursor()
 
-    cursor.execute(textwrap.dedent('''
-        CREATE TABLE tablaTest (
-            zona_inyeccion VARCHAR(50)
-        );
+    # Obtener zonas de inyeccion
+    cursor.execute(textwrap.dedent(''' 
+        SELECT zona_inyeccion, capacidad_base_firme FROM Tarifas;
     '''))
+    columnas = cursor.fetchall() # Guardar columnas en una lista
+    relacion = [0, 0, 0, 0]
+
+    # Los mapas están raros en python... esto en Java estaría pelado de hacer
+    # list(map(lambda x: print(x[0]), columnas))
+
+    # Se que hay una mejor manera de hacer esto, pero me da hueva investigar
+    for region in columnas:
+        match region[0]:
+            case 'Sur':         relacion[0] += region[1]
+            case 'Occidente':   relacion[1] += region[1]
+            case 'Golfo':       relacion[2] += region[1]
+            case 'Norte':       relacion[3] += region[1]
 
 
 if __name__ == '__main__':
